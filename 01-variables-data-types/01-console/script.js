861308e01c4a4cda43e96edae9f3b89f494b3674
emmauelrapoo <aniemmanuelrapo@gmail.com>
2023-09-09 02:40:28 +0100

    My Code Sanbox

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,25 @@
+console.log(100);
+
+console.log('Hello World')
+
+console.log(20, 'Hello', true)
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({ name: 'Rapoo', email: 'emmanuelrapoo.com' });
+
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'background-color:#fff; padding:10px; color:green;';
+
+console.log('%cHello World', styles)
