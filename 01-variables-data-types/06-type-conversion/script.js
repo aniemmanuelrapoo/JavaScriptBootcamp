ccbfa65024caff90cb900debeb5b99db84288916
emmauelrapoo <aniemmanuelrapo@gmail.com>
2023-09-11 14:31:45 +0100

    Type Conversion

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,22 @@
+let amount = 'Hello';
+
+// Convert String to number
+amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+// Convert number to a string
+// amount = amount.toString();
+// amount = String(amount);
+
+// Convert String to decimal
+// amount = parseFloat(amount);
+
+// Convert Number to Boolean
+// amount = Boolean(amount);
+console.log(Math.sqrt(-1));
+console.log(1 + NaN);
+console.log(undefined + undefined);
+console.log('foo' / 3)
+
+console.log(amount, typeof amount)
